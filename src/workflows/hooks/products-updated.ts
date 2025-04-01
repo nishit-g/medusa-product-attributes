ffbce4dc7cd97879926c3841cc9da51c3d269086
Nicolas Gorga <nicogorga11@gmail.com>
2025-04-01 14:41:55 -0300

    remove incorrectly placed hook handler

--- src/workflows/hooks/products-updated.ts
+++
@@ -1,8 +0,0 @@
-import { updateProductsWorkflow } from "@medusajs/medusa/core-flows";
-import { productsUpdatedHookHandler } from "../../utils/products-updated-handler";
-
-updateProductsWorkflow.hooks.productsUpdated(
-    async ({ products, additional_data }, { container }) => {
-        await productsUpdatedHookHandler({ products, additional_data, container})
-    }
-)
